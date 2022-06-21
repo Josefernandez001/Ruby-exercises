d7bf23081eec4506242612a608f4f4ef02f34513
Josefernandez001 <jose.fernandez@koombea.com>
2022-06-21 10:44:43 -0500

    corrections made

--- palindromo.rb
+++ palindromo.rb
@@ -1,10 +1,11 @@
-print  "Digite una palabra: "
-palabra = gets.chomp
-# esto de aqui es mas que todo por si se digita una frase palindroma 
-#por ejemplo  = dabale arroz a la zorra el abad
-#con el split las divido y las alamceno en un arreglo
-palabras = palabra.split(" ")
-#y con el join las uno todas en una sola
-palabras_unidas =palabras.join("")
-palabras_reverse = palabras_unidas.reverse
-puts  mensaje = if palabras_unidas.eql?(palabras_reverse) then "la palabra '#{palabra}' es palindromo" else  "la palabra #{palabra} no es palindromo "end
+print  "enter a word: "
+word = gets.chomp
+
+
+word_backwards = word.reverse
+
+puts message =  if word.eql?(word_backwards)
+    "la palabra '#{word}' es palindromo"
+else  
+    "la palabra #{word} no es palindromo "
+end
