e0dca324976d25bf32b3c4badcb5f436675fdad6
Josefernandez001 <107242533+Josefernandez001@users.noreply.github.com>
2022-06-22 16:22:23 -0500

    Update Largest_prime_factor.rb

--- Largest_prime_factor.rb
+++ Largest_prime_factor.rb
@@ -2,14 +2,14 @@
 number = 600851475143
 highest_prime_factor = 0
 
-x = 2
+increaser = 2
 
 while number != 1
-  if number % x == 0
-    highest_prime_factor = x if x > highest_prime_factor 
-    number = number / x
+  if number % increaser == 0
+    highest_prime_factor = increaser if increaser > highest_prime_factor 
+    number = number /increaser
   else
-    x += 1
+    increaser += 1
   end
 end
 puts "the largest prime factor is: #{highest_prime_factor}"
