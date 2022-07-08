734dbe9c395aa37e4123cd621d94e67a918dcbd2
Josefernandez001 <jose.fernandez@koombea.com>
2022-07-08 17:04:56 -0500

    class excersice product finished

---
+++ class/product.rb
@@ -0,0 +1,142 @@
+class Product
+  @@products = []
+  @@count = 0
+  def initialize(product)
+   @product = product
+  end
+
+  def count
+    @@count
+  end
+
+  def create
+    flag = nil
+    return 'id is required' if@product['id'].nil? ||@product['id'].eql?('')
+    return 'name is required' if@product['name'].nil? ||@product['name'].eql?('')
+    return 'value is required' if@product['value'].nil? ||@product['value'].eql?('')
+    return 'brand is required' if@product['brand'].nil? ||@product['brand'].eql?('')
+
+    if @@products.length >= 1
+      @@products.each do |product_obj|
+        flag = product_obj['id'].eql?@product['id']
+        return 'the id cannot be repeated' if flag.eql? true
+
+        break if flag.eql?(true)
+      end
+      if flag.eql?(false)
+        @@count += 1
+        @@products << @product
+        @product
+      end
+    else
+      @@count += 1
+      @@products <<@product
+      @product
+    end
+  end
+
+  def all
+    @@products
+  end
+
+  def find(id)
+    @@products.each do |product|
+      return product if product['id'] == id
+    end
+    'id not found'
+  end
+
+  def where(type,data)
+    responses = []
+    case type
+      when 'name'
+        @@products.each  do |product|
+          next if product['name'].nil?
+
+          responses << product if product['name'] == data
+        end
+      when 'brand'
+        @@products.each  do |product|
+          next if product['brand'].nil?
+
+          responses << product if product['brand'] == data
+        end
+      when 'description'
+        @@products.each do |product|
+          next if product['description'].nil?
+
+          responses << product if product['description'] == data
+        end
+      when 'value>='
+        @@products.each  do |product|
+          next if product['value'].nil?
+
+          responses << product if product['value'] >= data
+        end
+      when 'value<='
+        @@products.each  do |product|
+          next if product['value'].nil?
+
+          responses << product if product['value'] <= data
+        end
+      when 'quantity'
+        @@products.each do |product|
+          next if product['quantity'].nil?
+
+          responses << product if product['quantity'] == data
+        end
+      else
+        return 'op not found'
+      end
+    if responses.nil?.eql?(false) && responses.eql?([]) == false
+      responses
+    else
+      'not found'
+    end
+  end
+
+  def update(id,object)
+    return 'id is required' if object['id'].nil?.eql?(true) || object['id'].eql?('')
+    return 'name is required' if object['name'].nil?.eql?(true) || object['name'].eql?('')
+    return 'value is required' if object['value'].nil?.eql?(true) || object['value'].eql?('')
+    return 'brand is required' if object['brand'].nil?.eql?(true) || object['brand'].eql?('')
+
+    product_found = {}
+    @@products.each do |product|
+      product_found = product if product['id'] == id
+    end
+
+    return 'id not found' if product_found.nil?.eql?(true) || product_found.eql?({})
+
+    @@products.delete(product_found)
+
+    @@products.each do |product|
+      if product['id'].eql?(object['id'])
+        @@products << product_found
+        return 'that id is registered'
+      end
+    end
+    @@products << object
+    object
+  end
+
+  def delete(id)
+    product_found = {}
+    @@products.each do |product|
+      product_found = product if product['id'] == id
+    end
+
+    return 'id not found' if product_found.nil?.eql?(true) || product_found.eql?({})
+
+    @@products.delete(product_found)
+    product_found
+  end
+end
+# {'id' => 0,'name' => '', 'value' => 0, 'brand' => '', 'description' => '', 'quantity' => 0}
+# product = Product.new({'id' => 0,'name' => 'celphone', 'value' => 23239, 'brand' => 'motorola', 'description' => 'iphone14', 'quantity' => 16})
+# product.create
+# product1 = Product.new({'id' => 2,'name' => 'celphone', 'value' => 23239, 'brand' => 'apple', 'description' => 'iphone14', 'quantity' => 16})
+# product1.create
+# product.update(0,{'id' => 1,'name' => 'iphone', 'value' => 6_000_000, 'brand' => 'apple', 'description' => 'iphone14', 'quantity' => 100})
+
+# puts product.count
---
+++ class/spec/product_rspec.rb
@@ -0,0 +1,234 @@
+require_relative '../product'
+
+RSpec.describe Product do
+  subject{ described_class.new(product).create }
+  describe '#create' do
+    context 'when assessing positive cases' do
+      describe 'creation of a product' do
+        let(:product){{'id' => 0,'name' => 'air_spring', 'value' => 5_000, 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2}}
+        it { is_expected().to eq({'id' => 0,'name' => 'air_spring', 'value' => 5_000, 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2})}
+      end
+      describe 'creation of another product' do
+        let(:product){{'id' => 1,'name' => 'after_party', 'value' => 10_000, 'brand' => 'mumo', 'description' => 'helps to avoid hangover', 'quantity' => 100}}
+        it { is_expected().to eq({'id' => 1,'name' => 'after_party', 'value' => 10_000, 'brand' => 'mumo', 'description' => 'helps to avoid hangover', 'quantity' => 100})}
+      end
+    end
+
+    context 'when assessing negative cases' do
+      context 'when some required data is not passed' do
+        describe 'when the id is not passed' do
+        let(:product){{'name' => 'air_spring', 'value' => 5_000, 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2}}
+        it {is_expected().to eq('id is required')}
+        end
+
+        describe 'when the name is not passed' do
+        let(:product){{'id' => 0, 'value' => 5_000, 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2}}
+        it {is_expected().to eq('name is required')}
+        end
+
+        describe 'when the value is not passed' do
+          let(:product){{'id' => 0,'name' => 'air_spring', 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2}}
+          it {is_expected().to eq('value is required')}
+        end
+
+        describe 'when the brand is not passed' do
+          let(:product){{'id' => 0,'name' => 'air_spring', 'value' => 5_000, 'description' => 'sunglasses', 'quantity' => 2}}
+          it {is_expected().to eq('brand is required')}
+        end
+      end
+
+      describe 'when the id is repeated' do
+        let(:product){{'id' => 1,'name' => 'after_party', 'value' => 10_000, 'brand' => 'mumo', 'description' => 'helps to avoid hangover', 'quantity' => 100}}
+        it { is_expected().to eq('the id cannot be repeated')}
+      end
+    end
+  end
+# ------------
+  describe '#count' do
+    subject{ described_class.new('').count }
+    context 'when assessing positive cases' do
+      it {is_expected().to eq(2)}
+    end
+  end
+# ------------
+  describe '#all' do
+    subject{ described_class.new('').all }
+    context 'when assessing positive cases' do
+      describe 'should return [{}...]' do
+        it {is_expected.to eq([{'id' => 0,'name' => 'air_spring', 'value' => 5_000, 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2},{'id' => 1,'name' => 'after_party', 'value' => 10_000, 'brand' => 'mumo', 'description' => 'helps to avoid hangover', 'quantity' => 100}])}
+      end
+    end
+  end
+# ------------
+  describe '#find' do
+    subject{ described_class.new('').find(id) }
+    context 'when assessing positive cases' do
+      describe 'when the value of id is 0' do
+        let(:id){0}
+        it {is_expected.to eq({'id' => 0,'name' => 'air_spring', 'value' => 5_000, 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2})}
+      end
+
+      describe 'when the value of id is 2' do
+        let(:id){1}
+        it {is_expected.to eq({'id' => 1,'name' => 'after_party', 'value' => 10_000, 'brand' => 'mumo', 'description' => 'helps to avoid hangover', 'quantity' => 100})}
+      end
+    end
+
+    context 'when assessing negative cases' do
+      describe 'when the value of id is 10_000' do
+        let(:id){10_000}
+        it {is_expected.to eq('id not found')}
+      end
+
+      describe 'when the value of id is 20_000' do
+        let(:id){20_0001}
+        it {is_expected.to eq('id not found')}
+      end
+    end
+  end
+# ------------
+  describe '#where' do
+    subject{ described_class.new('').where(type,value) }
+    context 'when assessing positive cases' do
+      describe 'filtering by name ' do
+        let(:type){'name'}
+        let(:value){'after_party'}
+        it{is_expected.to eq([{'id' => 1,'name' => 'after_party', 'value' => 10_000, 'brand' => 'mumo', 'description' => 'helps to avoid hangover', 'quantity' => 100}])}
+      end
+
+      describe 'filtering by brand' do
+        let(:type){'brand'}
+        let(:value){'mumo'}
+        it{is_expected.to eq([{'id' => 1,'name' => 'after_party', 'value' => 10_000, 'brand' => 'mumo', 'description' => 'helps to avoid hangover', 'quantity' => 100}])}
+      end
+
+      describe 'filtering by description' do
+        let(:type){'description'}
+        let(:value){'sunglasses'}
+        it{is_expected.to eq([{'id' => 0,'name' => 'air_spring', 'value' => 5_000, 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2}])}
+      end
+
+      describe 'filtering by value>=' do
+        let(:type){'value>='}
+        let(:value){5_000}
+        it{is_expected.to eq([{'id' => 0,'name' => 'air_spring', 'value' => 5_000, 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2}, {'id' => 1,'name' => 'after_party', 'value' => 10_000, 'brand' => 'mumo', 'description' => 'helps to avoid hangover', 'quantity' => 100}])}
+      end
+
+      describe 'filtering by value<=' do
+        let(:type){'value<='}
+        let(:value){5_000}
+        it{is_expected.to eq([{'id' => 0,'name' => 'air_spring', 'value' => 5_000, 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2}])}
+      end
+
+      describe 'filtering by quantity' do
+        let(:type){'quantity'}
+        let(:value){2}
+        it{is_expected.to eq([{'id' => 0,'name' => 'air_spring', 'value' => 5_000, 'brand' => 'kuky', 'description' => 'sunglasses', 'quantity' => 2}])}
+      end
+    end
+
+    context 'when assessing negative cases' do
+      describe 'filtering by name ' do
+        let(:type){'name'}
+        let(:value){'alab'}
+        it{is_expected.to eq('not found')}
+      end
+
+      describe 'filtering by brand' do
+        let(:type){'brand'}
+        let(:value){'loop'}
+        it{is_expected.to eq('not found')}
+      end
+
+      describe 'filtering by description' do
+        let(:type){'description'}
+        let(:value){'pool '}
+        it{is_expected.to eq('not found')}
+      end
+
+      describe 'filtering by value>=' do
+        let(:type){'value>='}
+        let(:value){300000}
+        it{is_expected.to eq('not found')}
+      end
+
+      describe 'filtering by value<=' do
+        let(:type){'value<='}
+        let(:value){1}
+        it{is_expected.to eq('not found')}
+      end
+
+      describe 'filtering by quantity' do
+        let(:type){'quantity'}
+        let(:value){99}
+        it{is_expected.to eq('not found')}
+      end
+
+      describe 'when the value of type is incorrect' do
+        let(:type){'opfound'}
+        let(:value){99}
+        it{is_expected.to eq('op not found')}
+      end
+    end
+  end
+# ------------
+  describe '#update' do
+  subject{ described_class.new('').update(id,object) }
+    context 'when assessing positive cases' do
+      describe 'upgrading a product' do
+        let(:id){0}
+        let(:object){{'id' => 0,'name' => 'air_spring_v2', 'value' => 9_000, 'brand' => 'kuky', 'description' => 'sunglasses_v2', 'quantity' => 3}}
+        it{ is_expected.to eq({'id' => 0,'name' => 'air_spring_v2', 'value' => 9_000, 'brand' => 'kuky', 'description' => 'sunglasses_v2', 'quantity' => 3})}
+      end
+    end
+
+    context 'when assessing negative cases' do
+      context 'when some required data are not passed on' do
+        describe 'when not passed id' do
+          let(:id){0}
+          let(:object){{'name' => 'air_spring_v2', 'value' => 9_000, 'brand' => 'kuky', 'description' => 'sunglasses_v2', 'quantity' => 3}}
+          it{ is_expected.to eq('id is required')}
+        end
+        describe 'when not passed name' do
+          let(:id){0}
+          let(:object){{'id' => 0, 'value' => 9_000, 'brand' => 'kuky', 'description' => 'sunglasses_v2', 'quantity' => 3}}
+          it{ is_expected.to eq('name is required')}
+        end
+        describe 'when not passed brand' do
+          let(:id){0}
+          let(:object){{'id' => 0,'name' => 'air_spring_v2', 'value' => 9_000, 'description' => 'sunglasses_v2', 'quantity' => 3}}
+          it{ is_expected.to eq('brand is required')}
+        end
+        describe 'when not passed value' do
+          let(:id){0}
+          let(:object){{'id' => 0,'name' => 'air_spring_v2', 'brand' => 'kuky', 'description' => 'sunglasses_v2', 'quantity' => 3}}
+          it{ is_expected.to eq('value is required')}
+        end
+
+        describe 'when the id not found' do
+          let(:id){219_219}
+          let(:object){{'id' => 0,'name' => 'air_spring_v2', 'value' => 9_000, 'brand' => 'kuky', 'description' => 'sunglasses_v2', 'quantity' => 3}}
+          it{ is_expected.to eq('id not found')}
+        end
+      end
+    end
+  end
+# ------------
+  describe '#delete' do
+    subject{ described_class.new('').delete(id) }
+    context 'when assessing positive cases' do
+      describe 'when the value of id is 0' do
+        let(:id){0}
+        # here it returns this because the product with id 0 was modified earlier
+        it{is_expected.to eq({"brand"=>"kuky", "description"=>"sunglasses_v2", "id"=>0, "name"=>"air_spring_v2", "quantity"=>3, "value"=>9000})}
+      end
+    end
+
+    context 'when assessing negative cases' do
+      describe 'when the value of id is 8992' do
+        let(:id){8_992}
+        it{is_expected.to eq('id not found')}
+      end
+    end
+  end
+end
